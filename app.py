641ea70955a8f5ecc3f7376717d114f2f2ecfb57
fdiezr <felipediezchile@gmail.com>
2026-04-09 11:50:46 -0400

    Update app.py
    crear app limpia

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-version simple sin pdf
+from fastapi import FastAPI
+from openai import OpenAI
+import os
+
+app = FastAPI()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+@app.get("/")
+def inicio():
+    return {"mensaje": "API funcionando 🚀"}
+
+@app.get("/pregunta")
+def preguntar(q: str):
+
+    respuesta = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "Eres un abogado experto en derecho chileno."},
+            {"role": "user", "content": q}
+        ]
+    )
+
+    return {
+        "respuesta": respuesta.choices[0].message.content
+    }
